143da9fb1d026ab009d27e141dd360f442930f4d
Srushti Raybhoge <srushti.raybhoge@bfhl.in>
2023-04-03 14:16:58 +0530

    initial constant data added

--- src/views/Iwd.vue
+++ src/views/Iwd.vue
@@ -2,10 +2,16 @@
   <v-content class="pa-0">
     <v-container fluid class="mt-2 mb-0">
       <v-layout wrap align-start justify-center row fill-height class="my-0">
-        <v-flex xs12 md8 class="mb-0">
-          
-            
-
+        <v-flex xs12 md5 class="mb-0">
+          <span>GOOGLE DEVELOPERS</span>
+          <h1 class="google-font mb-0 mt-0" style="font-size:200%;color:#000000">Women Techmakers International Women's Day India Summit, 2023</h1>
+            <p style="font-size:120%;">International Women's Day Conference by Women Techmakers is back again this year after a brief pause owing to the pandemic. This year we look forward to welcome around 1,000 Women in Tech and Allies at our conference. The same will have Tech Talks, Panel Discussions and Fireside Chats followed by Networking Sessions. </p>
+            <p style="font-size:120%;">The theme for this year is DARE TO BE, empowering our community to have courage, to dream big and to be bold. #WTMDareToBe</p>
+            <p style="font-size:120%;" >Come join us on the 23rd of April, 2023 in Pune.</p>
+      
+        </v-flex>
+        <v-flex xs12 md5 class="mb-0">
+          <img src="background-image: url('http://localhost:8080/img/gdg.d65d8ad0.jpg'); background-position: center center;" alt="">
         </v-flex>
        
       </v-layout>
