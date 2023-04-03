ed40bc0995c0d4596a3a9da187cccdd79e64aca9
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2023-04-03 17:55:46 +0530

    Add Register Button

--- src/views/Iwd.vue
+++ src/views/Iwd.vue
@@ -4,24 +4,56 @@
       <v-layout wrap align-start justify-center row fill-height class="my-0">
         <v-flex xs12 md5 class="mb-0">
           <span>GOOGLE DEVELOPERS</span>
-          <h1 class="google-font mb-0 mt-0" style="font-size:200%;color:#000000">Women Techmakers International Women's Day India Summit, 2023</h1>
-            <p style="font-size:120%;">International Women's Day Conference by Women Techmakers is back again this year after a brief pause owing to the pandemic. This year we look forward to welcome around 1,000 Women in Tech and Allies at our conference. The same will have Tech Talks, Panel Discussions and Fireside Chats followed by Networking Sessions. </p>
-            <p style="font-size:120%;">The theme for this year is DARE TO BE, empowering our community to have courage, to dream big and to be bold. #WTMDareToBe</p>
-            <p style="font-size:120%;" >Come join us on the 23rd of April, 2023 in Pune.</p>
-      
+          <h1
+            class="google-font mb-0 mt-0"
+            style="font-size: 200%; color: #000000"
+          >
+            Women Techmakers International Women's Day India Summit, 2023
+          </h1>
+          <p style="font-size: 120%">
+            International Women's Day Conference by Women Techmakers is back
+            again this year after a brief pause owing to the pandemic. This year
+            we look forward to welcome around 1,000 Women in Tech and Allies at
+            our conference. The same will have Tech Talks, Panel Discussions and
+            Fireside Chats followed by Networking Sessions.
+          </p>
+          <p style="font-size: 120%">
+            The theme for this year is DARE TO BE, empowering our community to
+            have courage, to dream big and to be bold. #WTMDareToBe
+          </p>
+          <p style="font-size: 120%">
+            Come join us on the 23rd of April, 2023 in Pune.
+          </p>
         </v-flex>
         <v-flex xs12 md5 class="mb-0">
-          <img src="background-image: url('http://localhost:8080/img/gdg.d65d8ad0.jpg'); background-position: center center;" alt="">
+          <img
+            src="background-image: url('http://localhost:8080/img/gdg.d65d8ad0.jpg'); background-position: center center;"
+            alt=""
+          />
         </v-flex>
-       
+        <v-flex xs12 md10 class="">
+          <v-btn
+            href="https://bit.ly/iwdpune23"
+            target="_blank"
+            class="ma-0 google-font elevation-1"
+            color="#1a73e8"
+            style="text-transform: capitalize; border-radius: 5px; color: white"
+            >Register Now</v-btn
+          >
+          &nbsp;
+
+          <v-btn
+            target="_blank"
+            outline
+            color="cyan"
+            style="text-transform: capitalize; border-radius: 5px"
+            class="ml-0"
+            dark
+            >Call For Speakers</v-btn
+          >
+        </v-flex>
       </v-layout>
     </v-container>
-
-    
-
-
-
   </v-content>
-  
 </template>
 
