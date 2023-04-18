aff02691b3b359d942203c885389cab357c03c26
Srushti Raybhoge <srushti.raybhoge@bfhl.in>
2023-04-19 01:04:22 +0530

    table completed

--- src/views/Iwd.vue
+++ src/views/Iwd.vue
@@ -84,13 +84,173 @@
     <v-container fluid class="mt-1 mb-0">
       <v-layout wrap align-start row justify-center fill-height class="my-0">
         <v-flex xs12 md10 class="">
+          <!-- <img src="../assets/img/schedule.jpeg" width="60%" height="60%" class="margin-auto"/> -->
           <h1
-            class="google-font mb-0 mt-0"
-            style="font-size: 200%;"
+          class="google-font mb-0 mt-0"
+          style="font-size: 200%;"
           >
             Schedule
           </h1>
-          <img src="../assets/img/schedule.jpeg" width="60%" height="60%" class="margin-auto"/>
+        <v-container fluid style="border-color:#black;border-width: 1px;border-style: solid;border-radius:20px">
+          <v-layout wrap align-center justify-center row fill-height class="my-0">
+              <v-flex xs6 md5 class="mb-0" align-center justify-center style="text-align:center">
+                <p class="font-schedule">Registration</p>
+                <p class="font-schedule">9:00 am - 10:00 am</p>
+               </v-flex>
+               <v-flex xs6 md5 class="mb-0" align-center justify-center style="text-align:center">
+                <p class="font-schedule">Welcome Note</p>
+                <p class="font-schedule">10:00 am - 10:15 am</p>
+               </v-flex>
+          </v-layout>
+          <v-layout  wrap align-center justify-center  row fill-height>
+            <h2
+            class="google-font mb-0 mt-0"
+            style="font-size: 170%;"
+           
+          >
+            Agenda
+          </h2>
+        </v-layout>
+        <hr class="my-3"/>
+        <v-layout  wrap align-center justify-center  row fill-height>
+        <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+          <p class="font-schedule">10:15 am - 10:45 am</p>
+         </v-flex>
+         <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+          <p class="font-schedule">DareToBe!</p>
+         </v-flex>
+         <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+          <!-- Speaker -->
+         </v-flex>
+         </v-layout>
+         <v-layout  wrap align-center justify-center  row fill-height>
+          <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+            <p class="font-schedule">10:45 am - 11:15 am</p>
+           </v-flex>
+           <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+            <p class="font-schedule">Transitioning from a Developer to a Technical Leader</p>
+           </v-flex>
+           <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+            <!-- Speaker -->
+           </v-flex>
+           </v-layout>
+           <v-layout  wrap align-center justify-center  row fill-height>
+            <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+              <p class="font-schedule">11:15 am - 11:45 am</p>
+             </v-flex>
+             <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+              <p class="font-schedule">Where there is a Will there is a Way!</p>
+             </v-flex>
+             <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+              <!-- Speaker -->
+             </v-flex>
+             </v-layout>
+             <v-layout  wrap align-center justify-center  row fill-height>
+              <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                <p class="font-schedule">11:45 am - 12:15 pm</p>
+               </v-flex>
+               <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                <p class="font-schedule">Let's flow with Dataflow</p>
+               </v-flex>
+               <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                <!-- Speaker -->
+               </v-flex>
+               </v-layout>
+               <hr class="mt-2 mb-4"/>
+               <v-layout  wrap align-center justify-center  row fill-height>
+                <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                  <p class="font-schedule">12:15 pm - 01:00 pm</p>
+                 </v-flex>
+                 <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                  <p class="font-schedule">Activity</p>
+                 </v-flex>
+                 <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                  <!-- Speaker -->
+                 </v-flex>
+                 </v-layout>
+                 <hr class="mt-2 mb-4"/>
+               <v-layout  wrap align-center justify-center  row fill-height>
+                <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                  <p class="font-schedule">01:00 pm - 02:00 pm</p>
+                 </v-flex>
+                 <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                  <p class="font-schedule">Lunch</p>
+                 </v-flex>
+                 <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                  <!-- Speaker -->
+                 </v-flex>
+                 </v-layout>
+                 <hr class="mt-2 mb-4"/>
+                 <v-layout  wrap align-center justify-center  row fill-height>
+                  <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                    <p class="font-schedule">02:00 pm - 02:30 pm</p>
+                   </v-flex>
+                   <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                    <p class="font-schedule">Data Governance skills for Digital India</p>
+                   </v-flex>
+                   <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                    <!-- Speaker -->
+                   </v-flex>
+                   </v-layout>
+                   <v-layout  wrap align-center justify-center  row fill-height>
+                    <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                      <p class="font-schedule">02:30 pm - 03:00 pm</p>
+                     </v-flex>
+                     <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                      <p class="font-schedule">Big Data in the Financial Industry</p>
+                     </v-flex>
+                     <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                      <!-- Speaker -->
+                     </v-flex>
+                     </v-layout>
+                     <v-layout  wrap align-center justify-center  row fill-height>
+                      <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                        <p class="font-schedule">03:00 pm - 03:30 pm</p>
+                       </v-flex>
+                       <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                        <p class="font-schedule">Robotex india</p>
+                       </v-flex>
+                       <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                        <!-- Speaker -->
+                       </v-flex>
+                       </v-layout>
+                       <hr class="mt-2 mb-4"/>
+                       <v-layout  wrap align-center justify-center  row fill-height>
+                        <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                          <p class="font-schedule">03:30 pm - 03:45 pm</p>
+                         </v-flex>
+                         <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                          <p class="font-schedule">Hi Tea & Snacks</p>
+                         </v-flex>
+                         <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                          <!-- Speaker -->
+                         </v-flex>
+                         </v-layout>
+                         <hr class="mt-2 mb-4"/>
+                         <v-layout  wrap align-center justify-center  row fill-height>
+                          <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                            <p class="font-schedule">03:45 pm - 04:45 pm</p>
+                           </v-flex>
+                           <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                            <p class="font-schedule">Dare To Lead : A panel discussion with women in leadership</p>
+                           </v-flex>
+                           <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                            <!-- Speaker -->
+                           </v-flex>
+                           </v-layout>
+                           <hr class="mt-2 mb-4"/>
+                           <v-layout  wrap align-center justify-center  row fill-height>
+                            <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                              <p class="font-schedule">04:45 pm - 05:00 pm</p>
+                             </v-flex>
+                             <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                              <p class="font-schedule">Thanking Note</p>
+                             </v-flex>
+                             <v-flex xs3 md3 class="mb-0" align-center justify-center style="text-align:center">
+                              <!-- Speaker -->
+                             </v-flex>
+                             </v-layout>
+                            </v-container>
         </v-flex>
       </v-layout>
     </v-container>
